8703ffc3915710863f34dd956fbf79a309b340c5
Shaurya <shauryakumarsharma0007@gmail.com>
2024-03-21 02:01:14 +0530

    all operations implemented with hope so no bug! :)

--- Main.java
+++ Main.java
@@ -1,11 +1,46 @@
 import java.util.Scanner;
 
+class Banner
+{
+    Banner()
+    {
+        String banner = "Expense Tracker made by Shaurya Sharma";
+        int bannerLength = banner.length();
+
+        // Print top border
+        System.out.print("+");
+        for (int i = 0; i < bannerLength + 2; i++) {
+            System.out.print("-");
+        }
+        System.out.println("+");
+
+        // Print banner content
+        System.out.println("| " + banner + " |");
+
+        // Print bottom border
+        System.out.print("+");
+        for (int i = 0; i < bannerLength + 2; i++) {
+            System.out.print("-");
+        }
+        System.out.println("+");
+    }
+}
+
 class MainMenuNaive
 {
     static private int call()
     {
         int ch;
-        System.out.print("1) Insert Expense\n2)Update Expense\n3)Display Expense\n4)Delete Expense\n5)Quit\n> ");
+        System.out.println("+" + "-".repeat(36) + "+");
+        System.out.println("|        Select an option            |");
+        System.out.println("+" + "-".repeat(36) + "+");
+        System.out.println("| 1) Insert Expense                  |");
+        System.out.println("| 2) Update Expense                  |");
+        System.out.println("| 3) Display Expense                 |");
+        System.out.println("| 4) Delete Expense                  |");
+        System.out.println("| 5) Quit                            |");
+        System.out.println("+" + "-".repeat(36) + "+");
+        System.out.print("> ");
         Scanner sc = new Scanner(System.in);
         ch = sc.nextInt();
 
@@ -15,26 +50,33 @@ class MainMenuNaive
     {
         Operations ops = new Operations();
         Scanner sc = new Scanner(System.in);
+
+        Banner banner = new Banner();
+
         int ch = call();
-        while (ch != 5)
+        while (ch != 9)
         {
             try{
                 switch (ch) {
                     case 1:
+                        System.out.print("Expense Code(should be unique for each expense entered): ");
+                        Integer code = sc.nextInt();
+
+                        if (ops.codes.contains(code)){
+                            throw new CodeException();
+                        }
+
                         System.out.print("Expense name: ");
-                        String name = sc.nextLine();
+                        String name = sc.next();
 
                         System.out.print("Expense Amount: ");
-                        String amount = sc.nextLine();
+                        String amount = sc.next();
 
                         System.out.print("Expense Category: ");
-                        String cat = sc.nextLine();
+                        String cat = sc.next();
 
                         System.out.print("Expense Date: ");
-                        String date = sc.nextLine();
-
-                        System.out.print("Expense Code(should be unique for each expense entered): ");
-                        String code = sc.nextLine();
+                        String date = sc.next();
 
                         ops.InsertItem(name, cat, date, amount, code);
 
@@ -48,12 +90,29 @@ class MainMenuNaive
                         ops.Display();
                         ch = call();
                         break;
-                    default:
+                    case 4:
+                        ops.Delete();
+                        ch = call();
+                        break;
+                    case 7:
+                        ops.DEFAULT_DEMO();
+                        ch = call();
+                        break;
+                    case 5:
+                        System.out.println("+" + "-".repeat(45) + "+");
+                        System.out.println("|  Bye! See You Later.  |");
+                        System.out.println("+" + "-".repeat(45) + "+");
+                        ch = 9;
                         break;
                 }
             }catch (Exception e)
             {
-                System.out.println("invalid entry" + e.getMessage());
+                System.out.println("+------------------------------------+");
+                System.out.println("|          Error: Invalid Entry      |");
+                System.out.println("|                                    |");
+                System.out.println("| " + e.getMessage() + " |");
+                System.out.println("+------------------------------------+");
+                ch = call();
             }
         }
     }
